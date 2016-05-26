2d7e984670385832d46a8e044788394ef06251a3
Brian Tully <brian.tully@gmail.com>
2016-05-26 14:02:33 -0400

    theme changes to further aggregate CSS files (5 down to 2) using hook_css_alter

--- docroot/sites/all/themes/mayo/template.php
+++ docroot/sites/all/themes/mayo/template.php
@@ -257,3 +257,42 @@ function mayo_page_alter($page) {
   );
   drupal_add_html_head($viewport, "viewport");
 }
+
+/**
+ * Implements hook_css_alter().
+ */
+function mayo_css_alter(&$css) {
+
+  // Sort CSS items, so that they appear in the correct order.
+  // This is taken from drupal_get_css().
+  uasort($css, 'drupal_sort_css_js');
+
+  // The Print style sheets
+  // Populate this array with the print css
+  $print = array();
+
+  // Add weight to the new $css array
+  // so every element keeps its position
+  $weight = 0;
+
+  foreach ($css as $name => $style) {
+    // Leave conditional stylesheets untouched
+    // and put all the rest inside a 0 group
+    if ($css[$name]['browsers']['!IE']) {
+      $css[$name]['group'] = 0;
+      $css[$name]['weight'] = ++$weight;
+      $css[$name]['every_page'] = TRUE;
+    }
+
+    // Move all the print style sheets to a new array
+    if ($css[$name]['media'] == 'print') {
+      // remove and add to a new array
+      $print[$name] = $css[$name];
+      unset($css[$name]);
+    }
+
+  }
+
+  // Merge the regular array and the print array
+  $css = array_merge($css, $print);
+}
